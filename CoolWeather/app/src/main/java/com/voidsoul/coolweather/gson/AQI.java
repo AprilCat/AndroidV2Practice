bd702d50e46d39c94274ad12da9ca5487e9c3a58
AprilCat <604241857@qq.com>
2022-11-06 22:58:51 +0800

    add gson class

---
+++ CoolWeather/app/src/main/java/com/voidsoul/coolweather/gson/AQI.java
@@ -0,0 +1,11 @@
+package com.voidsoul.coolweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
